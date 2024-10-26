51c9b28456a19fde075b519a4083981a43de1807
mehedifiz <mehedi.shanto.contract@gmail.com>
2024-10-26 20:55:47 +0600

    set single  tourist spots

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ async function run() {
       res.send(result);
     });
 
+
+    app.get('/singleSpot/:id', async (req, res) => {
+      const id = new ObjectId(req.params.id);
+      const result = await touristSpotsCollection.findOne({ _id: id });
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
