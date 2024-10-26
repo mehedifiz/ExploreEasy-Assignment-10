aaae545b3516eeeee84a1cd7d32f9393dd5a260f
mehedifiz <mehedi.shanto.contract@gmail.com>
2024-10-26 21:40:08 +0600

    MongoDB allas connect

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config()
 const port = process.env.PORT || 5010;
-
+ 
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb://localhost:27017"; // MongoDB URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ensactw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`; 
+
+console.log()
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -87,12 +90,12 @@ async function run() {
     // await client.close();
   }
 }
-run().catch(console.dir);
+run().catch(console.dir); 
 
 app.get('/', (req, res) => {
-  res.send('Furry Travel server is running');
+  res.send(' Travel server is running');
 });
 
 app.listen(port, () => {
-  console.log(`Furry Travel server is running on port: ${port}`);
+  console.log(` Travel server is running on port: ${port}`);
 });
